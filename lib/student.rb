2240103a5035ae83dc13ac328db1b234e8b54a5d
Ian Mayton <ian.mayton@gmail.com>
2020-08-14 15:34:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -56,4 +56,15 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+  
+  def self.all_students_in_grade_9 
+    sql = <<-SQL 
+      SELECT * FROM students
+    SQL
+    
+    DB[:conn].execute(sql).collect do |row|
+      self.new_from_db(row)
+    end
+  end
+  
 end
