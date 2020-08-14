e17562a2b4d72a8d43c185d891f45879cd4e726f
Ian Mayton <ian.mayton@gmail.com>
2020-08-14 15:30:03 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -17,8 +17,11 @@ class Student
   def self.find_by_name(name)
     sql = <<-SQL 
     SELECT * FROM students 
-    
+    WHERE name = ?
+    LIMIT 1
     SQL
+    
+    DB[:conn].execute(sql, name)
   end
   
   def save
