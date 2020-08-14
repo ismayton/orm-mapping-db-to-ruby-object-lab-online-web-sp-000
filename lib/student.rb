5d4c16d9f2f9c191fe7f54a5b982f36e84508ee6
Ian Mayton <ian.mayton@gmail.com>
2020-08-14 15:38:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -92,6 +92,13 @@ class Student
   end
   
   def self.first_student_in_grade_10 
-    self.first_X_students_in_grade_10(1)
-  end
+    sql = <<-SQL 
+      SELECT * FROM students
+      WHERE grade = 10
+      LIMIT 1
+    SQL
+    
+    DB[:conn].execute(sql).collect do |row|
+      self.new_from_db(row)
+    end
 end
